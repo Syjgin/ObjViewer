7d3ee5ad0e37855dc523a4d4e85ccb371ea7492a
oleg.maximov@mobirate.com <Ymklajdet113>
2015-04-15 11:36:14 +0300

    basic model importing

---
+++ Assets/Scripts/ModelLoader.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ModelLoader : MonoBehaviour {
+
+    private const string ModelPath = "c:\\Users\\syjgin.MOBIRATE\\M9.obj";
+	void Start () {
+
+        Material diffuse = Resources.Load("Default-Material") as Material;
+        ObjImporter importer = new ObjImporter();
+        Mesh importedMesh = importer.ImportFile(ModelPath);
+        importedMesh.RecalculateNormals();
+        var importedObj = new GameObject("mesh", typeof(MeshRenderer), typeof(MeshFilter), typeof(MeshCollider));
+        importedObj.GetComponent<MeshFilter>().mesh = importedMesh;
+        importedObj.GetComponent<MeshRenderer>().materials[0] = diffuse;
+	}
+	
+
+}
