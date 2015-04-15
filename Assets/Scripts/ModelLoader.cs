d1203859bed5cb7771455556d610f97fa6fe57fe
Oleg Maximov <gruz103@gmail.com>
2015-04-15 19:45:14 +0300

    rotating completed

--- Assets/Scripts/ModelLoader.cs
+++ Assets/Scripts/ModelLoader.cs
@@ -3,21 +3,24 @@
 public class ModelLoader : MonoBehaviour {
 
     [SerializeField] private Material _diffuse;
+    [SerializeField] private Camera _camera;
 
     private const float MouseThreshold = 0.2f;
     private GameObject _importedObj;
+    private MeshCollider _collider;
 	void Start () {
 
         if(System.Environment.GetCommandLineArgs().Length > 1)
         {
-            string modelPath = "C:\\Users\\syjgin.MOBIRATE\\M9.obj";
-            //string modelPath = System.Environment.GetCommandLineArgs()[1];
+            //string modelPath = "C:\\Explorer\\teapot.obj";
+            string modelPath = System.Environment.GetCommandLineArgs()[1];
             ObjImporter importer = new ObjImporter();
             Mesh importedMesh = importer.ImportFile(modelPath);
             importedMesh.RecalculateNormals();
             _importedObj = new GameObject("mesh", typeof(MeshRenderer), typeof(MeshFilter), typeof(MeshCollider));
             _importedObj.GetComponent<MeshFilter>().mesh = importedMesh;
-            _importedObj.GetComponent<MeshRenderer>().materials[0] = _diffuse;   
+            _importedObj.GetComponent<MeshRenderer>().materials[0] = _diffuse;
+            _collider = _importedObj.GetComponent<MeshCollider>();
         }
 	}
 	
@@ -30,21 +33,31 @@ public class ModelLoader : MonoBehaviour {
 
              if (mousex > MouseThreshold)
              {
-                 Debug.Log("moved right");
+                 _importedObj.transform.Rotate(Vector3.up, -mousex*10, Space.World);
              }
              else if (mousex < -MouseThreshold)
              {
-                 Debug.Log("moved left");
+                 _importedObj.transform.Rotate(Vector3.up, -mousex * 10, Space.World);
              }
 
              if (mousey > MouseThreshold)
              {
-                 Debug.Log("moved up");
+                 _importedObj.transform.Rotate(Vector3.right, mousey * 10, Space.World);
              }
              else if (mousey < -MouseThreshold)
              {
-                 Debug.Log("moved down");
+                 _importedObj.transform.Rotate(Vector3.right, mousey * 10, Space.World);
              }   
+         }
+         if (Input.GetMouseButton(1) && _importedObj != null)
+         {
+             Ray ray = _camera.ScreenPointToRay (new Vector3(Input.mousePosition.x,Input.mousePosition.y,0));
+             RaycastHit hitInfo;
+             if (_collider.Raycast(ray, out hitInfo, 100))
+             {
+                 Debug.Log(hitInfo.point.x);
+                 Debug.Log(hitInfo.point.y);
+             }
          }
      }
 }
