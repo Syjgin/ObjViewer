c4257411a1daaa49dab53cc029cf57875f09d6ad
oleg.maximov@mobirate.com <Ymklajdet113>
2015-04-15 13:53:58 +0300

    command line args support

--- Assets/Scripts/ModelLoader.cs
+++ Assets/Scripts/ModelLoader.cs
@@ -1,20 +1,21 @@
-﻿using UnityEditor;
-using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+﻿using UnityEngine;
 
 public class ModelLoader : MonoBehaviour {
 
-    private const string ModelPath = "c:\\Users\\syjgin.MOBIRATE\\M9.obj";
+    [SerializeField] private Material _diffuse;
+    
 	void Start () {
 
-        Material diffuse = Resources.Load("Default-Material") as Material;
-        ObjImporter importer = new ObjImporter();
-        Mesh importedMesh = importer.ImportFile(ModelPath);
-        importedMesh.RecalculateNormals();
-        var importedObj = new GameObject("mesh", typeof(MeshRenderer), typeof(MeshFilter), typeof(MeshCollider));
-        importedObj.GetComponent<MeshFilter>().mesh = importedMesh;
-        importedObj.GetComponent<MeshRenderer>().materials[0] = diffuse;
+        if(System.Environment.GetCommandLineArgs().Length > 1)
+        {
+            string modelPath = System.Environment.GetCommandLineArgs()[1];//"C:\\Users\\syjgin.MOBIRATE\\M9.obj"
+            ObjImporter importer = new ObjImporter();
+            Mesh importedMesh = importer.ImportFile(modelPath);
+            importedMesh.RecalculateNormals();
+            var importedObj = new GameObject("mesh", typeof(MeshRenderer), typeof(MeshFilter), typeof(MeshCollider));
+            importedObj.GetComponent<MeshFilter>().mesh = importedMesh;
+            importedObj.GetComponent<MeshRenderer>().materials[0] = _diffuse;   
+        }
 	}
 	
 
